716461e6df31c40f4367999c75b5c6294b11a711
vvanegz <vvanegz@gmail.com>
2025-11-20 15:59:47 -0600

    contenido

--- src/Beneficiable.java
+++ src/Beneficiable.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
 public interface Beneficiable {
+    public double calcularBonificacion();
+    public double aplicarDescuentoSeguro();
 }
--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+public abstract class Empleado {
+    protected String id;
+    protected String nombre;
+    protected double salarioBase;
 
-public class Empleado {
+    public Empleado() {
+    }
+
+    public Empleado(String id, String nombre, double salarioBase) {
+        this.id = id;
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+    }
+
+    //getters y setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    //metodos
+    public abstract double calcularSalario();
+
+
+    public void mostrarInfo() {
+        System.out.println("ID: "+id);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Salario Base: "+salarioBase);
+    }
 }
